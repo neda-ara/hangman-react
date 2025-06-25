3d3f92f69acea78920ce5670ed2a21e975f22e09
Neda Ara <neda.ara9@gmail.com>
2025-06-26 02:40:02 +0530

    Add initial Hangman UI with figure and background

--- src/components/hangman-game.tsx
+++ src/components/hangman-game.tsx
@@ -1,10 +1,21 @@
-import { HangmanStickFigure } from "./hangman-stick-figure";
+import { HangmanFigure } from "./hangman-stick-figure";
 
 export const HangmanGame = () => {
   return (
-    <div className="min-h-100vh max-h-100vh">
-      <img alt="logo" className="h-15" src="src/assets/hangman-logo.svg" />
-      <HangmanStickFigure />
+    <div className="hangman-game-container min-h-100vh max-h-100vh flex flex-col items-center justify-between w-full h-screen">
+      <nav className="w-full">
+        <img alt="logo" className="h-14" src="src/assets/hangman-logo.svg" />
+      </nav>
+
+      <main className="flex-grow box-border w-full h-full px-10">
+        <HangmanFigure />
+      </main>
+
+      <footer className="flex justify-end w-full">
+        <p className="hangman-tagline uppercase text-lg tracking-widest text-white drop-shadow mb-2 px-4">
+          One rope. One word.
+        </p>
+      </footer>
     </div>
   );
 };
